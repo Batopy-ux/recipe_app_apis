8b348a05dc7664610a344d262e5748f98b839516
Prathamesh <prath37@gmail.com>
2025-06-15 15:56:15 +0530

    Added models and migrations for user

--- app/core/tests/test_command.py
+++ app/core/tests/test_command.py
@@ -2,9 +2,7 @@
 Test custom Django management commands.
 """
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2OpError
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -13,13 +11,10 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-
         call_command('wait_for_db')
-
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
@@ -29,6 +24,5 @@ class CommandTests(SimpleTestCase):
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
-
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
