8ce251b9dd534711962c69477368a040717241e1
Prathamesh <prath37@gmail.com>
2025-06-21 08:49:02 +0530

    update user api implemented

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -23,6 +23,16 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token"""
@@ -31,10 +41,11 @@ class AuthTokenSerializer(serializers.Serializer):
         style={'input_type': 'password'},
         trim_whitespace=False
     )
+
     def validate(self, attrs):
         """Validate and authenticate the users"""
         email = attrs.get('email')
-        password= attrs.get('password')
+        password = attrs.get('password')
         user = authenticate(
             request=self.context.get('request'),
             username=email,
